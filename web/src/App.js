4049f685097e17d78d95e1a317437d569b2bfca1
Tim Addison <git@tjaddison.com>
2020-03-17 21:21:34 -0400

    show tasks from api on load

--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const taskResult = await fetch("/api/tasks");
+      const tasksJson = await taskResult.json();
+      setTasks(tasksJson);
+    }
+    getTasks();
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {tasks.toString()}
     </div>
   );
 }
