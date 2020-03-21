d0e1d46fc1aad5f8b0d6e214bfa14d67c11340fa
Tim Addison <git@tjaddison.com>
2020-03-21 17:57:31 -0400

    get files recursively

--- api/src/index.js
+++ api/src/index.js
@@ -1,10 +1,27 @@
 const express = require("express");
 const fs = require("fs").promises;
+const { resolve } = require("path");
 
 const app = express();
 
+const getFiles = async directory => {
+  const directoryEntries = await fs.readdir(directory, { withFileTypes: true });
+  const files = await Promise.all(
+    directoryEntries.map(directoryEntry => {
+      const resolvedPath = resolve(directory, directoryEntry.name);
+
+      return directoryEntry.isDirectory()
+        ? getFiles(resolvedPath)
+        : resolvedPath;
+    })
+  );
+
+  return files.flat();
+};
+
 app.get("/api/tasks", async (req, res, next) => {
-  const files = await fs.readdir("/tasks");
+  const files = await getFiles("/tasks");
+
   res.status(200).json(files);
 });
 
